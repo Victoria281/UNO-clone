5d566c6f57df9cff0b7f03e38534c3b6a5a9840a
Victoria281 <amandaquek281@gmail.com>
2021-11-05 18:15:36 +0800

    Fixed missing files

---
+++ src/middlewares/printingRequest.js
@@ -0,0 +1,15 @@
+function printingDebugInfo(req, res, next) {
+    console.log("------------[Debug Info]---------------");
+    // console.log(`Servicing ${urlPattern}..`)
+    console.log("Servicing " + req.url + "..")
+
+    console.log("> req.params:" + JSON.stringify(req.params))
+    console.log("> req.body: " + JSON.stringify(req.body))
+    //console.log("> req.myOwnDebugInfo:" + JSON.stringify(req.myOwnDebugInfo))
+    console.log("------------[Debug Info]---------------");
+
+    console.log("")
+    next();
+}
+
+module.exports=printingDebugInfo;
