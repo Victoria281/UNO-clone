c744f8e66b40131c0ec20a4eb395fbcfa0759432
Victoria281 <amandaquek281@gmail.com>
2021-11-05 18:05:59 +0800

    Card API

--- src/controller/routes.js
+++ src/controller/routes.js
@@ -2,6 +2,11 @@ const createHttpError = require('http-errors');
 
 const app = require('express').Router();
 
+const bcrypt = require('bcrypt');
+const config = require('../../config');
+const jwt = require('jsonwebtoken');
+
+
 //ROUTES//
 const Card = require("../model/card")
 const User = require("../model/user")
@@ -12,14 +17,15 @@ const Auth = require("../model/auth")
 const printingDebuggingInfo = require("../middlewares/printingRequest");
 
 //=====================================
-//  LeaderBoard
+//  Card
 //=====================================
 
-//findByUserId
-app.get('/leaderboard/user/:id', printingDebuggingInfo, function (req, res, next) {
+//findById
+app.get('/card/:id', verifyToken, function (req, res, next) {
+    console.log("herere")
     const id = req.params.id;
 
-    LeaderBoard.findByUserId(id, function (err, result) {
+    Card.findByID(id, function (err, result) {
         if (err) {
             if (err.code === '23505') {
                 return next(createHttpError(404, `Not found`));
@@ -28,15 +34,15 @@ app.get('/leaderboard/user/:id', printingDebuggingInfo, function (req, res, next
                 return next(err);
             }
         } else {
-            return res.status(200).send({ score: result });
+            return res.json({ card: result });
         }
     });
 });
 
-//getAllScores
-app.get('/leaderboard', printingDebuggingInfo, function (req, res, next) {
+//findAll
+app.get('/cards', function (req, res, next) {
 
-    LeaderBoard.getAllScores(function (err, result) {
+    Card.findAll(function (err, result) {
         if (err) {
             if (err.code === '23505') {
                 return next(createHttpError(404, `Not found`));
@@ -45,66 +51,7 @@ app.get('/leaderboard', printingDebuggingInfo, function (req, res, next) {
                 return next(err);
             }
         } else {
-            return res.status(200).send({ score: result });
+            return res.json({ cards: result });
         }
     });
 });
-
-//getNumOfScores
-app.get('/leaderboard/:num', printingDebuggingInfo, function (req, res, next) {
-    const num = req.params.num;
-
-    LeaderBoard.getNumOfScores(num, function (err, result) {
-        if (err) {
-            if (err.code === '23505') {
-                return next(createHttpError(404, `Not found`));
-            }
-            else {
-                return next(err);
-            }
-        } else {
-            return res.status(200).send({ scores: result });
-        }
-    });
-});
-
-//updateHighestScore
-app.get('/leaderboard/update/:id', printingDebuggingInfo, function (req, res, next) {
-    const id = req.params.id;
-    const score = req.body.score;
-
-    LeaderBoard.updateHighestScore(score, id, function (err, result) {
-        if (err) {
-            if (err.code === '23505') {
-                return next(createHttpError(404, `Not found`));
-            }
-            else {
-                return next(err);
-            }
-        } else {
-            return res.status(201).send({result: "Created"});
-        }
-    });
-});
-
-//insertNewScore
-app.post('/leaderboard/insert/:id', printingDebuggingInfo, function (req, res, next) {
-    const id = req.params.id;
-    const score = req.body.score;
-
-    LeaderBoard.insertNewScore(id, score, function (err, result) {
-        if (err) {
-            if (err.code === '23505') {
-                return next(createHttpError(404, `Not found`));
-            }
-            else {
-                return next(err);
-            }
-        } else {
-            return res.status(201).send({result: "Created"});
-        }
-    });
-});
-
-
-module.exports = app;
