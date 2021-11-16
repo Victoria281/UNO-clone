72d8434f16dc372bb476852800ca4c2ba7ca3aa1
Victoria281 <amandaquek281@gmail.com>
2021-11-16 12:51:53 +0800

    Added navbar design

--- client/src/App.js
+++ client/src/App.js
@@ -9,12 +9,13 @@ import AccountPage from "./pages/account";
 import RegisterPage from "./pages/register";
 import ProfilePage from "./pages/profile";
 import LeaderboardPage from "./pages/leaderboard";
+import { NavLink } from 'react-router-dom'
 
 export default function App() {
   return (
     <Router>
       <div>
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <nav className="navbar navbar-expand-sm navbar-light">
           <button
             className="navbar-toggler"
             type="button"
@@ -26,28 +27,41 @@ export default function App() {
           >
             <span className="navbar-toggler-icon"></span>
           </button>
-          <p className="brand">UNO Clone</p>
+          <p className="brand d-none d-sm-block">
+            <div class="card1"></div>
+            <div class="card2"></div>
+            <p class="logomain">NOU</p>
+            <p class="logosub">uno-clone</p>
+          </p>
           <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
             <ul className="navbar-nav ml-auto mt-2 mt-lg-0">
-              <li className="nav-item active navbarDesign">
-                <p className="nav-link navBarWord">
-                  <Link to="/leaderboard">Ranks</Link>
-                </p>
+              <li className="nav-item active navbarDesign" style={{ background: '#e71e1e' }}>
+
+                <NavLink to="/leaderboard" exact activeClassName="activeIcon"> <div className="borderHover" style={{ borderColor: '#e71e1e' }}><p className="nav-link navBarWord">
+                  Ranks
+                </p></div></NavLink>
+
               </li>
-              <li className="nav-item active">
-                <p className="nav-link">
-                  <Link to="/">Game</Link>
-                </p>
+              <li className="nav-item active navbarDesign" style={{ background: '#1E9FE7' }}>
+
+                <NavLink to="/" exact activeClassName="activeIcon"><div className="borderHover" style={{ borderColor: '#1E9FE7' }}><p className="nav-link navBarWord">
+                  Game
+                </p></div></NavLink>
+
               </li>
-              <li className="nav-item active">
-                <p className="nav-link">
-                  <Link to="/profile">Profile</Link>
-                </p>
+              <li className="nav-item active navbarDesign" style={{ background: '#46E71E' }}>
+
+                <NavLink to="/profile" exact activeClassName="activeIcon"><div className="borderHover" style={{ borderColor: '#46E71E' }}><p className="nav-link navBarWord">
+                  Profile
+                </p></div></NavLink>
+
               </li>
-              <li className="nav-item active">
-                <p className="nav-link">
-                  <Link to="/login">Login</Link>
-                </p>
+              <li className="nav-item active navbarDesign" style={{ background: '#F5F93C' }}>
+
+                <NavLink to="/login" exact activeClassName="activeIcon"><div className="borderHover" style={{ borderColor: '#F5F93C' }}><p className="nav-link navBarWord">
+                  Login
+                </p></div></NavLink>
+
               </li>
             </ul>
           </div>
