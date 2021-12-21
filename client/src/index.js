6e8336482ae7b6dcafae0a26f9f2615ca96f33f7
Wong Wei Jian <wwj051102@gmail.com>
2021-12-21 20:36:32 +0800

    added @ts-nocheck to the top of a few files

--- client/src/index.js
+++ client/src/index.js
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
@@ -6,7 +7,9 @@ import rootReducers from "./store/reducer/index";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 
-const store = createStore(rootReducers);
+// @ts-ignore
+const store = createStore(rootReducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
 ReactDOM.render(
   <Provider store={store}>
     <App />
