66d1500faf3abbcb9bcb3c06822b3801ba73dc52
Wong Wei Jian <wwj051102@gmail.com>
2021-12-25 11:07:11 +0800

    alter sql statement to not pick out null values when the 2 tables do not match

--- src/model/leaderboard.js
+++ src/model/leaderboard.js
@@ -33,7 +33,7 @@ var LeaderBoard = {
     getNumOfScores: function (num, callback) {
         const query = {
             name: 'getNumOfScores',
-            text: 'SELECT score, uno_leaderboard.created_by, username, userid, profileicon FROM uno_leaderboard RIGHT JOIN players using (userid) WHERE score IS NOT NULL AND uno_leaderboard.created_by IS NOT NULL ORDER BY uno_leaderboard.score DESC LIMIT $1;',
+            text: 'SELECT score, uno_leaderboard.created_by, username, uno_leaderboard.userid, profileicon FROM uno_leaderboard LEFT JOIN players ON uno_leaderboard.userid = players.userid ORDER BY uno_leaderboard.score DESC LIMIT $1;',
             values: [num],
         }
 
