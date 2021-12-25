46995248d2c36b7cf87db52806efa17eb2abc714
Wong Wei Jian <wwj051102@gmail.com>
2021-12-25 21:58:04 +0800

    fix leaderboard graph spacing, fix leaderboard showing userid instead of rank, fix not showing all the players before the actual player not showing in the list, fix padding on a few objects

--- client/src/components/OtherPlayers.js
+++ client/src/components/OtherPlayers.js
@@ -2,36 +2,40 @@
 import "../css/leaderboard.css";
 
 const OtherPlayers = ({ users }) => {
+    console.log("users:", users);
     return (
         <div>
             {users.map((players, index) => {
+                console.log("index:" + index);
                 if (parseInt(players.userid) === parseInt(localStorage.getItem("userid"))) {
-                    return (<div className="row no-gutters leaderboard_player">
-                        <div className="col-sm-2 leaderboard_col text-center">
-                            <p className=" font-weight-bold p-2">{players.userid}</p>
-                        </div>
-                        <div className="col-sm-5 leaderboard_col">
-                            <div className="row no-gutters">
-                                <div className="col-2 p-2">
-                                    <img
-                                        className="img-responsive lb2Icons"
-                                        alt="pic"
-                                        src={"https://uno-clone.herokuapp.com/api/uno/profile_icons/" + players.profileicon + ".png"}
-                                    />
-                                </div>
-                                <div className="col-10 py-2 px-3">
-                                    <p className="p-2">{players.username}</p>
+                    return (
+                        <div className="row no-gutters leaderboard_player">
+                            <div className="col-sm-2 leaderboard_col text-center">
+                                <p className=" font-weight-bold p-2">{index + 1}</p>
+                            </div>
+                            <div className="col-sm-5 leaderboard_col">
+                                <div className="row no-gutters">
+                                    <div className="col-2 p-2">
+                                        <img
+                                            className="img-responsive lb2Icons"
+                                            alt="pic"
+                                            src={"https://uno-clone.herokuapp.com/api/uno/profile_icons/" + players.profileicon + ".png"}
+                                        />
+                                    </div>
+                                    <div className="col-10 py-2 px-3">
+                                        <p className="p-2">{players.username}</p>
+                                    </div>
                                 </div>
                             </div>
+                            <div className="col-sm-2 leaderboard_col py-2">
+                                <p className="p-2">{players.score}</p>
+                            </div>
+                            <div className="col-sm-3 leaderboard_col py-2">
+                                <p className="p-1 pr-2 createdDate">{(players.created_by).split("T")[0]}</p>
+                            </div>
                         </div>
-                        <div className="col-sm-2 leaderboard_col py-2">
-                            <p className="p-2">{players.score}</p>
-                        </div>
-                        <div className="col-sm-3 leaderboard_col py-2">
-                            <p className="p-2">{players.created_by}</p>
-                        </div>
-                    </div>)
-                } else if (index > 3) {
+                    );
+                } else if (index >= 3) {
                     return (
                         <div className="row no-gutters leaderboard_row">
                             <div className="col-sm-2 p-2 leaderboard_col text-center">
@@ -55,14 +59,36 @@ const OtherPlayers = ({ users }) => {
                                 <p className="p-2">{players.score}</p>
                             </div>
                             <div className="col-sm-3 leaderboard_col py-2">
-                                <p className="p-2">{players.created_by}</p>
+                                <p className="p-1 pr-2 createdDate">{(players.created_by).split("T")[0]}</p>
                             </div>
                         </div>
-                    )
+                    );
                 } else {
                     return (
-                        <div>
-                            <p>Something went wrong</p>
+                        <div className="row no-gutters leaderboard_row">
+                            <div className="col-sm-2 p-2 leaderboard_col text-center">
+                                <p className="font-weight-bold">{index + 1}</p>
+                            </div>
+                            <div className="col-sm-5 leaderboard_col">
+                                <div className="row no-gutters">
+                                    <div className="col-2 p-2">
+                                        <img
+                                            className="img-responsive lb2Icons"
+                                            alt="pic"
+                                            src={"https://uno-clone.herokuapp.com/api/uno/profile_icons/" + players.profileicon + ".png"}
+                                        />
+                                    </div>
+                                    <div className="col-10 py-2 px-3">
+                                        <p className="p-2">{players.username}</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-sm-2 leaderboard_col py-2">
+                                <p className="p-2">{players.score}</p>
+                            </div>
+                            <div className="col-sm-3 leaderboard_col py-2">
+                                <p className="p-1 pr-2 createdDate">{(players.created_by).split("T")[0]}</p>
+                            </div>
                         </div>
                     );
                 }
--- client/src/pages/leaderboard.js
+++ client/src/pages/leaderboard.js
@@ -51,71 +51,62 @@ const Leaderboard = () => {
                 <div className="gameBody">
                     {ifConfetti ? <Confetti run={ifConfetti} width={window.innerWidth * 0.6} height={window.innerHeight} /> : null}
                     <div className="row no-gutters">
-                        <div className="col-7 p-5">
-                            <div id="podium" className="row no-gutters">
-                                <div className="col-4">
-                                    <div className="h-25"> </div>
-                                    <div id="podiumPillar2">
-                                        <div className="lb2IconBorder">
-                                            <img className="img-responsive lb1Icons" alt="pic" src={process.env.REACT_APP_API_URL + "/api/uno/profile_icons/" + p2.profileicon + ".png"} />
-                                        </div>
-                                        <div className="borderDesign2">
-                                            <div className="d-flex justify-content-end">
-                                                <div id="triangle2"></div>
-                                            </div>
-                                            <p className="lbuserheader">{p2.username}</p>{" "}
-                                            <p className="lbuser">
-                                                <strong>{p2.score} pts</strong>
-                                            </p>
-                                        </div>
+                        <div className="col-7 p-5 mb-6">
+                            <div id="podium" className="row no-gutters pillarBody">
+                                <div className="col-4 p-2" id="podiumPillar2">
+                                    <div className="lb2IconBorder">
+                                        <img className="img-responsive lb1Icons" alt="pic" src={process.env.REACT_APP_API_URL + "/api/uno/profile_icons/" + p2.profileicon + ".png"} />
                                     </div>
-                                </div>
-
-                                <div id="firsplace" className="col-4">
-                                    <div id="podiumPillar1">
+                                    <div className="borderDesign2">
                                         <div className="d-flex justify-content-end">
-                                            <img id="crown" className="img-responsive" alt="pic" src={crownImage} />
+                                            <div id="triangle2"></div>
                                         </div>
-
-                                        <div className="lb1IconBorder">
-                                            <img className="img-responsive lb1Icons" alt="pic" src={process.env.REACT_APP_API_URL + "/api/uno/profile_icons/" + p1.profileicon + ".png"} />
-                                        </div>
-                                        <div className="borderDesign1">
-                                            <div className="d-flex justify-content-end">
-                                                <div id="triangle1"></div>
-                                            </div>
-                                            <p className="lbuserheader">
-                                                {p1.username}
-                                            </p>
-                                            <p className="lbuser">
-                                                <strong>{p1.score}pts</strong>
-                                            </p>
-                                        </div>
+                                        <p className="lbuserheader">{p2.username}</p>{" "}
+                                        <p className="lbuser">
+                                            <strong>{p2.score} pts</strong>
+                                        </p>
                                     </div>
                                 </div>
 
-                                <div className="col-4">
-                                    <div className="h-50"> </div>
-                                    <div id="podiumPillar3">
-                                        <div className="lb3IconBorder">
-                                            <img className="img-responsive lb1Icons" alt="pic" src={process.env.REACT_APP_API_URL + "/api/uno/profile_icons/" + p3.profileicon + ".png"} />
+                                <div className="col-4 p-2" id="podiumPillar1">
+                                    <div className="d-flex justify-content-end">
+                                        <img id="crown" className="img-responsive" alt="pic" src={crownImage} />
+                                    </div>
+
+                                    <div className="lb1IconBorder">
+                                        <img className="img-responsive lb1Icons" alt="pic" src={process.env.REACT_APP_API_URL + "/api/uno/profile_icons/" + p1.profileicon + ".png"} />
+                                    </div>
+                                    <div className="borderDesign1">
+                                        <div className="d-flex justify-content-end">
+                                            <div id="triangle1"></div>
                                         </div>
-                                        <div className="borderDesign3">
-                                            <div className="d-flex justify-content-end">
-                                                <div id="triangle3"></div>
-                                            </div>
-                                            <p className="lbuserheader">{p3.username}</p>{" "}
-                                            <p className="lbuser">
-                                                <strong>{p3.score}pts</strong>
-                                            </p>
-                                        </div>
+                                        <p className="lbuserheader">
+                                            {p1.username}
+                                        </p>
+                                        <p className="lbuser">
+                                            <strong>{p1.score}pts</strong>
+                                        </p>
                                     </div>
                                 </div>
 
+                                <div className="col-4 p-2" id="podiumPillar3" >
+                                    <div className="lb3IconBorder">
+                                        <img className="img-responsive lb1Icons" alt="pic" src={process.env.REACT_APP_API_URL + "/api/uno/profile_icons/" + p3.profileicon + ".png"} />
+                                    </div>
+                                    <div className="borderDesign3">
+                                        <div className="d-flex justify-content-end">
+                                            <div id="triangle3"></div>
+                                        </div>
+                                        <p className="lbuserheader">{p3.username}</p>{" "}
+                                        <p className="lbuser">
+                                            <strong>{p3.score}pts</strong>
+                                        </p>
+                                    </div>
+                                </div>
                             </div>
                         </div>
 
-                        <div className="col-5">
+                        <div className="col-5 pt-3">
                             <div className="leaderboard_body">
                                 <div className="row no-gutters">
                                     <div className="col-sm-2 leaderboard_col text-center">
