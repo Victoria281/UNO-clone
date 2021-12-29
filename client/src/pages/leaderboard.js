c833c4daa5c79ca65465fb481f8513105283d009
Wong Wei Jian <wwj051102@gmail.com>
2021-12-29 19:43:58 +0800

    implemented tab viewing for ranks page to show leaderboard and my stats, account functionality yet to be added

--- client/src/pages/leaderboard.js
+++ client/src/pages/leaderboard.js
@@ -7,15 +7,17 @@ import { LoadingScreen } from "../components/loadingScreen";
 const OtherPlayers = React.lazy(() => import("../components/OtherPlayers"));
 
 export default class Leaderboard extends Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         this.state = {
             users: [],
             ifConfetti: true,
             p1: {},
             p2: {},
-            p3: {}
-        }
+            p3: {},
+            selectedTab: 'leaderboard',
+        };
+        this.changeSelectedTab = this.changeSelectedTab.bind(this);
     }
 
     componentDidMount() {
@@ -60,95 +62,206 @@ export default class Leaderboard extends Component {
         return ifConfetti ? <Confetti run="true" id="confettiLeaderboard" width={window.innerWidth * 0.6} height={window.innerHeight} onConfettiComplete={this.stopConfetti()} /> : null;
     }
 
-    render() {
+    changeSelectedTab(tab) {
+        console.log("original_inState:", this.state.selectedTab);
+        console.log("new_toChange:", tab);
+
+        switch (tab) {
+            case "leaderboard":
+                this.setState({ selectedTab: "leaderboard" });
+                console.log("after:", this.state.selectedTab);
+                break;
+            case "stats":
+                this.setState({ selectedTab: "stats" });
+                this.setState({ ifConfetti: true });
+                console.log("after:", this.state.selectedTab);
+                break;
+            default:
+                this.setState({ selectedTab: "leaderboard" });
+                console.log("after:", this.state.selectedTab);
+                break;
+        }
+    }
+
+    tab() {
+        return (
+            <div className="tabContainer no-gutters">
+                <div className="tabSelector">
+                    <div className="ldb">
+                        <button type="button" className="btn btn-info" onClick={() => this.changeSelectedTab('leaderboard')}>Leaderboard</button>
+                        {/* <p className="tabText">Leaderboard</p> */}
+                    </div>
+                    <div className="stats">
+                        <button type="button" className="btn btn-info" onClick={() => this.changeSelectedTab('stats')}>My Stats</button>
+                        {/* <p className="tabText">My Stats</p> */}
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
+    leaderboard() {
         const { users, p1, p2, p3, ifConfetti } = this.state;
+
+
         return (
             <Fragment>
-                {users.length === 0 || (
-                    <div className="gameBody">
-                        {this.checkConfettiStats(ifConfetti)}
-                        <div className="row no-gutters">
-                            <div className="col-xl-7 col-lg-7 col-md-12 col-sm-12 p-5 mb-6">
-                                <div id="podium" className="row no-gutters pillarBody">
-                                    <div className="col-4 p-2" id="podiumPillar2">
-                                        <div className="lb2IconBorder">
-                                            <img className="img-responsive lb1Icons" alt="pic" src={process.env.REACT_APP_API_URL + "/api/uno/profile_icons/" + p2.profileicon + ".png"} />
-                                        </div>
-                                        <div className="borderDesign2">
-                                            <div className="d-flex justify-content-end">
-                                                <div id="triangle2"></div>
-                                            </div>
-                                            <p className="lbuserheader">{p2.username}</p>{" "}
-                                            <p className="lbuser">
-                                                <strong>{p2.score} pts</strong>
-                                            </p>
-                                        </div>
+                <div className="gameBody">
+                    <div id="activeContent" className="row no-gutters">
+                        <div className="col-xl-7 col-lg-7 col-md-12 col-sm-12 p-5 mb-6">
+                            {this.checkConfettiStats(ifConfetti)}
+                            <div id="podium" className="row no-gutters pillarBody">
+                                <div className="col-4 p-2" id="podiumPillar2">
+                                    <div className="lb2IconBorder">
+                                        <img className="img-responsive lb1Icons" alt="pic" src={process.env.REACT_APP_API_URL + "/api/uno/profile_icons/" + p2.profileicon + ".png"} />
                                     </div>
-
-                                    <div className="col-4 p-2" id="podiumPillar1">
+                                    <div className="borderDesign2">
                                         <div className="d-flex justify-content-end">
-                                            <img id="crown" className="img-responsive" alt="pic" src={crownImage} />
+                                            <div id="triangle2"></div>
                                         </div>
-
-                                        <div className="lb1IconBorder">
-                                            <img className="img-responsive lb1Icons" alt="pic" src={process.env.REACT_APP_API_URL + "/api/uno/profile_icons/" + p1.profileicon + ".png"} />
-                                        </div>
-                                        <div className="borderDesign1">
-                                            <div className="d-flex justify-content-end">
-                                                <div id="triangle1"></div>
-                                            </div>
-                                            <p className="lbuserheader">
-                                                {p1.username}
-                                            </p>
-                                            <p className="lbuser">
-                                                <strong>{p1.score}pts</strong>
-                                            </p>
-                                        </div>
-                                    </div>
-
-                                    <div className="col-4 p-2" id="podiumPillar3" >
-                                        <div className="lb3IconBorder">
-                                            <img className="img-responsive lb1Icons" alt="pic" src={process.env.REACT_APP_API_URL + "/api/uno/profile_icons/" + p3.profileicon + ".png"} />
-                                        </div>
-                                        <div className="borderDesign3">
-                                            <div className="d-flex justify-content-end">
-                                                <div id="triangle3"></div>
-                                            </div>
-                                            <p className="lbuserheader">{p3.username}</p>{" "}
-                                            <p className="lbuser">
-                                                <strong>{p3.score}pts</strong>
-                                            </p>
-                                        </div>
+                                        <p className="lbuserheader">{p2.username}</p>{" "}
+                                        <p className="lbuser">
+                                            <strong>{p2.score} pts</strong>
+                                        </p>
                                     </div>
                                 </div>
-                            </div>
 
-                            <div className="col-xl-5 col-lg-5 col-md-12 col-sm-12 pt-3">
-                                <h4 className="text-center">Leaderboard</h4>
-                                <div className="leaderboard_body">
-                                    <div className="row no-gutters">
-                                        <div className="col-sm-2 leaderboard_col text-center">
-                                            <h6 className="p-1 font-weight-bold">No</h6>
-                                        </div>
-                                        <div className="col-sm-5 leaderboard_col">
-                                            <h6 className="p-1 font-weight-bold">Players</h6>
-                                        </div>
-                                        <div className="col-sm-2 leaderboard_col">
-                                            <h6 className="p-1 font-weight-bold">Score</h6>
-                                        </div>
-                                        <div className="col-sm-3 leaderboard_col">
-                                            <h6 className="p-1 font-weight-bold">Created</h6>
-                                        </div>
+                                <div className="col-4 p-2" id="podiumPillar1">
+                                    <div className="d-flex justify-content-end">
+                                        <img id="crown" className="img-responsive" alt="pic" src={crownImage} />
                                     </div>
-                                    <Suspense fallback={<LoadingScreen />}>
-                                        <OtherPlayers users={users} />
-                                    </Suspense>
+
+                                    <div className="lb1IconBorder">
+                                        <img className="img-responsive lb1Icons" alt="pic" src={process.env.REACT_APP_API_URL + "/api/uno/profile_icons/" + p1.profileicon + ".png"} />
+                                    </div>
+                                    <div className="borderDesign1">
+                                        <div className="d-flex justify-content-end">
+                                            <div id="triangle1"></div>
+                                        </div>
+                                        <p className="lbuserheader">
+                                            {p1.username}
+                                        </p>
+                                        <p className="lbuser">
+                                            <strong>{p1.score}pts</strong>
+                                        </p>
+                                    </div>
+                                </div>
+
+                                <div className="col-4 p-2" id="podiumPillar3" >
+                                    <div className="lb3IconBorder">
+                                        <img className="img-responsive lb1Icons" alt="pic" src={process.env.REACT_APP_API_URL + "/api/uno/profile_icons/" + p3.profileicon + ".png"} />
+                                    </div>
+                                    <div className="borderDesign3">
+                                        <div className="d-flex justify-content-end">
+                                            <div id="triangle3"></div>
+                                        </div>
+                                        <p className="lbuserheader">{p3.username}</p>{" "}
+                                        <p className="lbuser">
+                                            <strong>{p3.score}pts</strong>
+                                        </p>
+                                    </div>
                                 </div>
                             </div>
                         </div>
+
+                        <div className="col-xl-5 col-lg-5 col-md-12 col-sm-12 pt-3">
+                            <h4 className="text-center">Leaderboard</h4>
+                            <div className="leaderboard_body">
+                                <div className="row no-gutters">
+                                    <div className="col-sm-2 leaderboard_col text-center">
+                                        <h6 className="p-1 font-weight-bold">No</h6>
+                                    </div>
+                                    <div className="col-sm-5 leaderboard_col">
+                                        <h6 className="p-1 font-weight-bold">Players</h6>
+                                    </div>
+                                    <div className="col-sm-2 leaderboard_col">
+                                        <h6 className="p-1 font-weight-bold">Score</h6>
+                                    </div>
+                                    <div className="col-sm-3 leaderboard_col">
+                                        <h6 className="p-1 font-weight-bold">Created</h6>
+                                    </div>
+                                </div>
+                                <Suspense fallback={<LoadingScreen />}>
+                                    <OtherPlayers users={users} />
+                                </Suspense>
+                            </div>
+                        </div>
                     </div>
-                )}
+                </div>
             </Fragment>
-        );
+        )
+    }
+
+    stats() {
+        const { users } = this.state;
+        return (
+            <Fragment>
+                <div className="gameBody">
+                    <div id="activeContent" className="row no-gutters">
+                        <div className="col-xl-5 col-lg-5 col-md-12 col-sm-12 pt-3">
+                            <h4 className="text-center">Leaderboard</h4>
+                            <div className="leaderboard_body">
+                                <div className="row no-gutters">
+                                    <div className="col-sm-2 leaderboard_col text-center">
+                                        <h6 className="p-1 font-weight-bold">No</h6>
+                                    </div>
+                                    <div className="col-sm-5 leaderboard_col">
+                                        <h6 className="p-1 font-weight-bold">Players</h6>
+                                    </div>
+                                    <div className="col-sm-2 leaderboard_col">
+                                        <h6 className="p-1 font-weight-bold">Score</h6>
+                                    </div>
+                                    <div className="col-sm-3 leaderboard_col">
+                                        <h6 className="p-1 font-weight-bold">Created</h6>
+                                    </div>
+                                </div>
+                                <Suspense fallback={<LoadingScreen />}>
+                                    <OtherPlayers users={users} />
+                                </Suspense>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </Fragment>
+        )
+    }
+
+    render() {
+        const { users, selectedTab } = this.state;
+
+        if (selectedTab === "leaderboard") {
+            return (
+                <Fragment>
+                    {users.length === 0 || (
+                        <div>
+                            {this.tab()}
+                            {this.leaderboard()}
+                        </div>
+                    )}
+                </Fragment>
+            );
+        } else if (selectedTab === "stats") {
+            return (
+                <Fragment>
+                    {users.length === 0 || (
+                        <div>
+                            {this.tab()}
+                            {this.stats()}
+                        </div>
+                    )}
+                </Fragment>
+            );
+        } else {
+            return (
+                <Fragment>
+                    {users.length === 0 || (
+                        <div>
+                            {this.tab()}
+                        </div>
+                    )}
+                </Fragment>
+            );
+        }
+
     }
 }
--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,9 +1,8 @@
-// @ts-check
+// @ts-nocheck
 import rootReducers from "./reducer/index";
 import { createStore } from '@reduxjs/toolkit';
 
 export const store = createStore(
     rootReducers,
-    // @ts-ignore
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
