012bc50f1bc3ddf1a74982d72edb24602b00e73f
Wong Wei Jian <wwj051102@gmail.com>
2022-01-11 23:04:57 +0800

    Styling changes

--- client/src/Component/Leaderboard/Ranks/Leaderboard.js
+++ client/src/Component/Leaderboard/Ranks/Leaderboard.js
@@ -1,14 +1,14 @@
 import { useSelector } from 'react-redux';
 import { useState } from 'react';
 // import { useDispatch } from 'react-redux';
-import Confetti from 'react-confetti';
+// import Confetti from 'react-confetti';
 import { Suspense } from 'react';
 
 // Type Imports
 import { RootState } from '../../../store/types';
 
 // MUI Material Library Imports
-import { Typography, Box, Avatar } from '@mui/material';
+import { Typography, Box, Avatar, Button } from '@mui/material';
 
 // CSS Module Imports
 import styles from '../styles.module.css';
@@ -18,6 +18,8 @@ import crownImage from "../../../icons/pepicons_crown.png";
 import OtherPlayers from './OtherPlayers';
 import { LoadingScreen } from '../../loadingScreen';
 import { updateCurrentUserStats } from '../../../store/action/leaderboard/stats';
+// import confetti from 'canvas-confetti'
+import Confetti from 'react-canvas-confetti';
 
 const DisplayLeaderboard = () => {
     const player1 = useSelector(
@@ -59,86 +61,117 @@ const DisplayLeaderboard = () => {
 
     const [showConfetti, setShowConfetti] = useState(true);
 
-    const TheConfetti = () => {
-        console.log("TheConfetti is Running", showConfetti);
+    const canvasStyles = {
+        position: 'fixed',
+        pointerEvents: 'none',
+        width: '100%',
+        height: '100%',
+        top: 0,
+        left: 0,
+    };
 
-        if (showConfetti) {
-            console.log("TheConfetti is Running");
-            return (
-                <Confetti width={window.innerWidth * 0.6} height={window.innerHeight} />
-            );
 
-        } else {
-            console.log("TheConfetti is Not Running");
-            return null;
-        }
+
+    let animationInstance = null;
+
+    let makeShot = (particleRatio, opts) => {
+        animationInstance && animationInstance({
+            ...opts,
+            origin: { y: 0.7 },
+            particleCount: Math.floor(200 * particleRatio),
+        });
     }
 
-    setTimeout(() => {
-        console.log("running state change");
-        setShowConfetti(false);
-    }, 5000);
-    
+    let fire = () => {
+        makeShot(0.25, {
+            spread: 26,
+            startVelocity: 55,
+        });
+
+        makeShot(0.2, {
+            spread: 60,
+        });
+
+        makeShot(0.35, {
+            spread: 100,
+            decay: 0.91,
+            scalar: 0.8,
+        });
+
+        makeShot(0.1, {
+            spread: 120,
+            startVelocity: 25,
+            decay: 0.92,
+            scalar: 1.2,
+        });
+
+        makeShot(0.1, {
+            spread: 120,
+            startVelocity: 45,
+        });
+
+        makeShot(0.1, {
+            spread: 120,
+            startVelocity: 45,
+        });
+    };
+
+    let handleFire = () => {
+        fire();
+    };
+
+    let getInstance = (instance) => {
+        animationInstance = instance;
+    };
+
+
     return (
         <Box className={`row no-gutters ${styles.gameBody}`}>
 
             <Box className='col-xl-7 col-lg-7 col-md-12 col-sm-12'>
-                <TheConfetti />
-                <Box className={styles.ldbPodium}>
-                    <Box className={styles.pillarBody}>
-                        <Box className='row no-gutters'>
-
-                            <Box className={styles.podiumPillar2}>
-                                <Box className='col-4 p-2'>
-                                    <Box className={styles.lb2IconBorder}>
-                                        <Avatar className={styles.lb1Icons} alt="2ndPlace" src={process.env.REACT_APP_API_URL + "/api/uno/profile_icons/" + player2.profileicon + ".png"}></Avatar>
-                                    </Box>
-                                    <Box className={styles.borderDesign2}>
-                                        <Box alignContent={'end'} className='d-flex'>
-                                            <Box className={styles.podiumTriangle2}></Box>
-                                        </Box>
-                                        <Typography className={styles.lbuserheader}>{player2.username}</Typography>
-                                        <Typography className={styles.lbuser} sx={{ fontWeight: 'bold' }}>{player2.score} pts</Typography>
-                                    </Box>
-                                </Box>
+                <Box className={`row no-gutters ${styles.ldbPodium} ${styles.pillarBody}`} alignContent={'end'} textAlign={'end'}>
+                    <Box className={`col-sm-4 p-2 ${styles.podiumPillar2}`}>
+                        <Box className={styles.lb2IconBorder}>
+                            <Avatar className={styles.lb1Icons} alt="2ndPlace" src={process.env.REACT_APP_API_URL + "/api/uno/profile_icons/" + player2.profileicon + ".png"}></Avatar>
+                        </Box>
+                        <Box className={styles.borderDesign2}>
+                            <Box alignContent={'end'} className='d-flex'>
+                                <Box className={styles.podiumTriangle2}></Box>
                             </Box>
+                            <Typography className={styles.lbuserheader}>{player2.username}</Typography>
+                            <Typography className={styles.lbuser} sx={{ fontWeight: 'bold' }}>{player2.score} pts</Typography>
+                        </Box>
+                    </Box>
 
-                            <Box className={styles.podiumPillar1}>
-                                <Box className='col-4 p-2'>
-                                    <Box alignContent={'end'} className='d-flex'>
-                                        <Avatar className={styles.crown} alt="1stPlace" src={crownImage}></Avatar>
-                                    </Box>
+                    <Box className={`col-sm-4 p-2 ${styles.podiumPillar1}`}>
+                        <Box alignContent={'end'} className='d-flex'>
+                            <Avatar className={styles.crown} alt="1stPlace" src={crownImage}></Avatar>
+                        </Box>
 
-                                    <Box className={styles.lb1IconBorder}>
-                                        <Avatar className='lb1Icons' alt='1stPlace' src={process.env.REACT_APP_API_URL + "/api/uno/profile_icons/" + player1.profileicon + ".png"}></Avatar>
-                                    </Box>
+                        <Box className={styles.lb1IconBorder}>
+                            <Avatar className='lb1Icons' alt='1stPlace' src={process.env.REACT_APP_API_URL + "/api/uno/profile_icons/" + player1.profileicon + ".png"}></Avatar>
+                        </Box>
 
-                                    <Box className={styles.borderDesign1}>
-                                        <Box alignContent={'end'} className='d-flex'>
-                                            <Box className={styles.podiumTriangle1}></Box>
-                                        </Box>
-                                        <Typography className={styles.lbuserheader}>{player1.username}</Typography>
-                                        <Typography className={styles.lbuser} sx={{ fontWeight: 'bold' }}>{player1.score} pts</Typography>
-                                    </Box>
-                                </Box>
+                        <Box className={styles.borderDesign1}>
+                            <Box alignContent={'end'} className='d-flex'>
+                                <Box className={styles.podiumTriangle1}></Box>
                             </Box>
+                            <Typography className={styles.lbuserheader}>{player1.username}</Typography>
+                            <Typography className={styles.lbuser} sx={{ fontWeight: 'bold' }}>{player1.score} pts</Typography>
+                        </Box>
+                    </Box>
 
-                            <Box className={styles.podiumPillar3}>
-                                <Box className='col-4 p-2'>
+                    <Box className={`col-sm-4 p-2 ${styles.podiumPillar3}`}>
+                        <Box className={styles.lb3IconBorder}>
+                            <Avatar className='lb1Icons' alt='3rdPlace' src={process.env.REACT_APP_API_URL + "/api/uno/profile_icons/" + player3.profileicon + ".png"}></Avatar>
+                        </Box>
 
-                                    <Box className={styles.lb3IconBorder}>
-                                        <Avatar className='lb1Icons' alt='3rdPlace' src={process.env.REACT_APP_API_URL + "/api/uno/profile_icons/" + player3.profileicon + ".png"}></Avatar>
-                                    </Box>
-
-                                    <Box className={styles.borderDesign3}>
-                                        <Box alignContent={'end'} className='d-flex'>
-                                            <Box className={styles.podiumTriangle3}></Box>
-                                        </Box>
-                                        <Typography className={styles.lbuserheader}>{player3.username}</Typography>
-                                        <Typography className={styles.lbuser} sx={{ fontWeight: 'bold' }}>{player3.score} pts</Typography>
-                                    </Box>
-                                </Box>
+                        <Box className={styles.borderDesign3}>
+                            <Box alignContent={'end'} className='d-flex'>
+                                <Box className={styles.podiumTriangle3}></Box>
                             </Box>
+                            <Typography className={styles.lbuserheader}>{player3.username}</Typography>
+                            <Typography className={styles.lbuser} sx={{ fontWeight: 'bold' }}>{player3.score} pts</Typography>
                         </Box>
                     </Box>
                 </Box>
