27a2e41a420de490b088e440ba3bc4fff85820a2
Victoria281 <amandaquek281@gmail.com>
2022-01-20 16:35:34 +0800

    Revert "HomePage Animation"
    This reverts commit 83be33d706b5fa30cdad6a762c2e4c163a26098f.

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,58 +1,22 @@
 import "../css/home.css";
-import { Grid, styled, Paper, Typography } from '@mui/material';
-import { homeAnimation } from "./homeAnimation";
-import { useEffect } from "react";
-import { typography } from "@mui/system";
 
-const Item = styled(Paper)(({ theme }) => ({
-  ...theme.typography.body2,
-  padding: theme.spacing(2),
-  textAlign: 'center',
-  color: theme.palette.text.secondary,
-  margin: theme.spacing(4),
-  borderTopRightRadius: 50,
-  borderBottomRightRadius: 50,
-  ":hover": {
-    transform: 'scale(1.05,1.05)'
-  }
-}));
-
-const Home = () => {
-  useEffect(() => {
-    homeAnimation()
-  }, [])
+export default function App() {
 
   return (
-    <Grid container spacing={2} className="App">
-      <Grid item xs={0.5}></Grid>
-      <Grid item xs={6} sx={{ mt: 10 }}>
-        <Typography variant="h1">Uno Clone</Typography>
-        <Item sx={{ bgcolor: 'info.main' }}>
-          <a className="startBtn" href="./game">
-            Single Player
-          </a>
-        </Item>
-        <Item sx={{ bgcolor: 'secondary.main' }}>
-          <a className="startMultiBtn" href="./createroom">
-            Multiplayer
-          </a>
-        </Item>
-        <Item sx={{ bgcolor: 'error.main' }}>
-          <a className="leaderBoardBtn" href="./leaderboard">
-            LeaderBoard
-          </a>
-        </Item>
-        <Item sx={{ bgcolor: 'success.main' }}>
-          <a className="profileBtn" href="./profile">
-            Profile
-          </a>
-        </Item>
-      </Grid>
-    </Grid>
-    // <div>
-    //   hihrvieu
-    // </div>
+    <div className="App">
+      <div className="cardsHome">
+          <img className="cardImage1" alt="wildImg" src={process.env.REACT_APP_API_URL + "/api/uno/images/Wild.png"} />
+          <img className="cardImage2" alt="wildDrawImg" src={process.env.REACT_APP_API_URL + "/api/uno/images/Wild_Draw.png"} />
+      </div>
+
+      <div className="row d-flex justify-content-between">
+        <a className="startBtn" href="./game">
+          Single Player
+        </a>
+        <a className="startMultiBtn" href="./createroom">
+          Multiplayer
+        </a>
+      </div>
+    </div>
   );
 }
-
-export default Home;
