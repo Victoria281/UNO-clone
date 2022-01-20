cb14cd22413cde0bf1bde572897fc9e84a8ca03c
Victoria281 <amandaquek281@gmail.com>
2022-01-20 16:35:32 +0800

    Revert "Updated Homepage"
    This reverts commit d24e89259c4312f0868487d11878349548bc8fb2.

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -2,19 +2,19 @@ import "../css/home.css";
 import { Grid, styled, Paper, Typography } from '@mui/material';
 import { homeAnimation } from "./homeAnimation";
 import { useEffect } from "react";
+import { typography } from "@mui/system";
 
 const Item = styled(Paper)(({ theme }) => ({
   ...theme.typography.body2,
   padding: theme.spacing(2),
   textAlign: 'center',
   color: theme.palette.text.secondary,
-  margin: theme.spacing(2),
+  margin: theme.spacing(4),
   borderTopRightRadius: 50,
   borderBottomRightRadius: 50,
   ":hover": {
     transform: 'scale(1.05,1.05)'
-  },
-  width: '75%'
+  }
 }));
 
 const Home = () => {
@@ -24,9 +24,9 @@ const Home = () => {
 
   return (
     <Grid container spacing={2} className="App">
-      <Grid item xs={1}></Grid>
-      <Grid item xs={6}>
-        <Typography variant="h1" sx={{ mt: 10,mb:10 }}>Uno Clone</Typography>
+      <Grid item xs={0.5}></Grid>
+      <Grid item xs={6} sx={{ mt: 10 }}>
+        <Typography variant="h1">Uno Clone</Typography>
         <Item sx={{ bgcolor: 'info.main' }}>
           <a className="startBtn" href="./game">
             Single Player
@@ -49,6 +49,9 @@ const Home = () => {
         </Item>
       </Grid>
     </Grid>
+    // <div>
+    //   hihrvieu
+    // </div>
   );
 }
 
