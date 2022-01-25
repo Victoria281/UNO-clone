fcaa33d5af0de6b4a64a9660ffc53dd16a31b98b
Aisyah129 <aisyahnimanah7.12@gmail.com>
2022-01-25 11:31:36 +0800

    Merging development

--- client/src/App.js
+++ client/src/App.js
@@ -23,12 +23,9 @@ import Loader from "./Component/OtherComponents/LoadingComponent/Loader"
 //new
 import MultiplayerCreateRoom from "./Component/MultiplayerComponents/createRoom"
 import MultiplayerGameRoom from "./Component/MultiplayerComponents/gameRoom"
-<<<<<<< HEAD
 import WaitingRoom from "./pages/waitingRoom";
 
-=======
 import DefaultNavBar from "./Component/OtherComponents/NavigationBar/DefaultNavBar"
->>>>>>> 6c572c31657578193569f50fada70c6657f17d9f
 const socket = io.connect(process.env.REACT_APP_API_URL);
 
 function AppGameRoom(props) {
@@ -59,82 +56,6 @@ const App = ({ hideLoader }) => {
 
   return (
     <Router>
-<<<<<<< HEAD
-      <div>
-        <nav className="navbar navbar-expand-sm navbar-light">
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarTogglerDemo03"
-            aria-controls="navbarTogglerDemo03"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <p className="brand d-none d-sm-block">
-            <div className="card1"></div>
-            <div className="card2"></div>
-            <p className="logomain">NOU</p>
-            <p className="logosub">uno-clone</p>
-          </p>
-          <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
-            <ul className="navbar-nav ml-auto mt-2 mt-lg-0">
-              <li className="nav-item active navbarDesign" style={{ background: '#e71e1e' }}>
-
-                <NavLink to="/leaderboard" exact activeClassName="activeIcon"> <div className="borderHover" style={{ borderColor: '#e71e1e' }}><p className="nav-link navBarWord">
-                  Ranks
-                </p></div></NavLink>
-
-              </li>
-              <li className="nav-item active navbarDesign" style={{ background: '#1E9FE7' }}>
-
-                <NavLink to="/" exact activeClassName="activeIcon"><div className="borderHover" style={{ borderColor: '#1E9FE7' }}><p className="nav-link navBarWord">
-                  Game
-                </p></div></NavLink>
-
-              </li>
-              <li className="nav-item active navbarDesign" style={{ background: '#46E71E' }}>
-
-                <NavLink to="/profile" exact activeClassName="activeIcon"><div className="borderHover" style={{ borderColor: '#46E71E' }}><p className="nav-link navBarWord">
-                  Profile
-                </p></div></NavLink>
-
-              </li>
-              <li className="nav-item active navbarDesign" style={{ background: '#F5F93C' }}>
-                <Account isLoggedIn={loggedIn}/>
-              </li>
-              <li className="nav-item active navbarDesign" style={{ background: '#FFB967' }}>
-                <div className="borderHover" style={{ borderColor: '#FFB967' }}><p className="nav-link navBarWord">
-                <Music/>
-                </p></div>
-              </li>
-            </ul>
-          </div>
-        </nav>
-
-        <Switch>
-          <PageRestriction exact path="/" component={HomePage} />
-          <PageRestriction exact path="/game" component={GamePage} />
-          <PageRestriction exact path="/end" component={EndPage} />
-          <Route exact path="/login" component={AccountPage} />
-          <Route exact path="/register" component={RegisterPage} />
-          <PageRestriction exact path="/profile" component={ProfilePage} />
-          <PageRestriction exact path="/leaderboard" component={LeaderboardPage} />
-          <Route exact path="/logout" component={Logout} />
-          {/* <PageRestriction exact path="/createroom" component={Room} socket={socket}/> */}
-          {/* <PageRestriction path="/multiplayer/:roomname/:username" component={Appmain} socket={socket}/> */}
-
-          {/* new */}
-          <Route exact path="/createroom" render={()=><MultiplayerCreateRoom socket={socket}/>}/>
-          <Route path="/multiplayer/:roomcode" component={AppGameRoom}/>
-          <Route exact path="/waitingroom" component={WaitingRoom} />
-
-        </Switch>
-      </div>
-    </Router>
-=======
       <Switch>
         <DefaultNavBar exact path="/" component={HomePage} loggedIn={loggedIn}/>
         <PageRestriction exact path="/load" component={Loader} />
@@ -154,7 +75,6 @@ const App = ({ hideLoader }) => {
         <Route path="/multiplayer/:roomcode" component={AppGameRoom} />
       </Switch>
     </Router >
->>>>>>> 6c572c31657578193569f50fada70c6657f17d9f
   );
 }
 function Logout() {
