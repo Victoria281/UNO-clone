8d6a892084889b0966763632892225ca76757b4a
Maryse <Maryse.akbl@gmail.com>
2022-01-28 15:33:10 +0800

    Added determinate progress bar

--- client/src/Component/OtherComponents/NotificationComponent/Notifications.js
+++ client/src/Component/OtherComponents/NotificationComponent/Notifications.js
@@ -1,6 +1,9 @@
 import { Snackbar, LinearProgress, Alert, Box, Slide } from '@mui/material';
+import { useState, useEffect } from 'react';
 
 const CustomNotification = ({ notif, setNotif }) => {
+
+    const [progress, setProgress] = useState(0);
 
     function handleClose() {
         console.log("Snackbar Closed")
@@ -11,19 +14,35 @@ const CustomNotification = ({ notif, setNotif }) => {
         return <Slide {...props} direction="down"/>;
     }
 
+    useEffect(() => {
+        const timer = setInterval(() => {
+          setProgress((oldProgress) => {
+            if (oldProgress === 100) {
+              return 0;
+            }
+            const diff = Math.random() * 10;
+            return Math.min(oldProgress + diff, 100);
+          });
+        }, 500);
+    
+        return () => {
+          clearInterval(timer);
+        };
+      }, []);
+
     return (
         <Snackbar
             anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
             open={notif.open}
             onClose={handleClose}
-            autoHideDuration={3000}
+            autoHideDuration={5000}
             TransitionComponent={TransitionDown}
         >
              <Box sx={{ width: '100%'}}>   
                  <Alert onClose={handleClose} severity={notif.type} sx={{ width: '100%' }}>
                     {notif.message}
                 </Alert>
-                    <LinearProgress color={notif.type} sx={{ width: '100%' }} />
+                    <LinearProgress color={notif.type} sx={{ width: '100%' }} variant="determinate" value={progress} />
                 </Box>
         </Snackbar>
     )
