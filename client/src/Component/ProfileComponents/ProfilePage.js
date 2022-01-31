a81a84b0339f99d8bb060a14b0355a21ae89b247
Victoria281 <amandaquek281@gmail.com>
2022-01-31 16:12:01 +0800

    changed profile update

--- client/src/Component/ProfileComponents/ProfilePage.js
+++ client/src/Component/ProfileComponents/ProfilePage.js
@@ -8,7 +8,7 @@ import Friends from './FriendsSection/Friends'
 import FindFriends from './FriendsSection/FindFriends'
 import PendingFriends from './FriendsSection/PendingFriends'
 import { useDispatch, useSelector } from 'react-redux'
-import {Box} from '@mui/material'
+import { Box } from '@mui/material'
 import AccountBoxIcon from '@mui/icons-material/AccountBox';
 import LockIcon from '@mui/icons-material/Lock';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
@@ -29,7 +29,7 @@ const Profile = () => {
 
   useEffect(() => {
     const uid = localStorage.getItem('userid')
-    if(uid !== undefined){
+    if (uid !== undefined) {
       dispatch(getUserInfo(uid));
     }
     console.log('profile_state returned')
@@ -40,25 +40,25 @@ const Profile = () => {
     o_setNotif(data)
   }
 
-  const handleEditUserInfo = (data) =>{
+  const handleEditUserInfo = (data) => {
     setEditUserInfo(data)
   }
 
- 
+
   return (
     <Fragment>
       {
         //profile_state.userInfo.length === 0 
         profile_state.userInfo == undefined
-        ?
+          ?
           <p>Loading</p>
           :
-          <Box id="root">
+          <Box className={styles.Profile}>
             <CustomNotification uopen={o_Notif} usetOpen={o_setNotif} />
 
-            <ProfileModal 
-            setErrorNotif={setErrorNotif}
-            profile_state={profile_state}
+            <ProfileModal
+              setErrorNotif={setErrorNotif}
+              profile_state={profile_state}
             />
             <Box className="gameProfileBody py-4">
               <Box className="row no-gutters">
@@ -67,16 +67,16 @@ const Profile = () => {
                   <Box className="card my-3">
                     <Box id="headerOne" className="card-header d-flex justify-content-between" data-toggle="collapse" href="#collapseOne">
                       <button className="collapsed card-link text-dark">
-                        <AccountBoxIcon/>    Profile
+                        <AccountBoxIcon />    Profile
                       </button>
-                      <KeyboardArrowDownIcon/>{/* <i className="fa fa-arrow-down p-1"></i> */}
+                      <KeyboardArrowDownIcon />{/* <i className="fa fa-arrow-down p-1"></i> */}
                     </Box>
                     <Box id="collapseOne" className="collapse show" data-parent="#accordion">
                       <Box id="bodyOne" className="card-body">
-                        <UserInfoCard 
-                        edit={editUserInfo}
-                        handleEditUserInfo={handleEditUserInfo}
-                        setErrorNotif={setErrorNotif}/>
+                        <UserInfoCard
+                          edit={editUserInfo}
+                          handleEditUserInfo={handleEditUserInfo}
+                          setErrorNotif={setErrorNotif} />
                       </Box>
                     </Box>
                   </Box>
@@ -85,66 +85,66 @@ const Profile = () => {
                   <Box className="card my-3">
                     <Box id="headerTwo" className="card-header d-flex justify-content-between" data-toggle="collapse" href="#collapseTwo">
                       <button className="collapsed card-link text-dark" >
-                        <LockIcon/>    Change Password
+                        <LockIcon />    Change Password
                       </button>
-                      <KeyboardArrowDownIcon/>
+                      <KeyboardArrowDownIcon />
                     </Box>
                     <Box id="collapseTwo" className="changePassword collapse" data-parent="#accordion">
                       <Box id="bodyTwo" className="card-body">
-                        <SecurityCard setErrorNotif={setErrorNotif}/>
+                        <SecurityCard setErrorNotif={setErrorNotif} />
                       </Box>
                     </Box>
                   </Box>
 
-               {/* Friends */}
-              <div className={`card my-3 ${styles.accordionBox}`}>
-                <div id="headerThree" className="card-header d-flex justify-content-between" data-toggle="collapse" href="#collapseThree">
-                  <button className="collapsed card-link text-dark">
-                    <i className="fa fa-user-o"></i>    My Friends
-                  </button>
-                  <KeyboardArrowDownIcon/>
-                </div>
-                <div id="collapseThree" className={`collapse ${styles.accordionBoxBody}`} data-parent="#accordion">
-                  <div id="bodyThree" className={`card-body`}>
-                    <Friends />
+                  {/* Make Friend Requests */}
+                  <div className={`card my-3 ${styles.accordionBox}`}>
+                    <div id="headerFive" className="card-header d-flex justify-content-between" data-toggle="collapse" href="#collapseFive">
+                      <button className="collapsed card-link text-dark">
+                        <i className="fa fa-user-o"></i>    Find Friends
+                      </button>
+                      <KeyboardArrowDownIcon />
+                    </div>
+                    <div id="collapseFive" className={`collapse ${styles.accordionBoxBody}`} data-parent="#accordion">
+                      <div id="bodyFive" className={`card-body`}>
+                        <FindFriends />
+                      </div>
+                    </div>
                   </div>
-                </div>
-              </div>
 
-              {/* Pending Friend Requests */}
-              <div className={`card my-3 ${styles.accordionBox}`}>
-                <div id="headerFour" className="card-header d-flex justify-content-between" data-toggle="collapse" href="#collapseFour">
-                  <button className="collapsed card-link text-dark">
-                    <i className="fa fa-user-o"></i>    My Pending Friend Requests
-                  </button>
-                  <KeyboardArrowDownIcon/>
-                </div>
-                <div id="collapseFour" className={`collapse ${styles.accordionBoxBody}`} data-parent="#accordion">
-                  <div id="bodyFour" className={`card-body`}>
-                    <PendingFriends />
+                  {/* Friends */}
+                  <div className={`card my-3 ${styles.accordionBox}`}>
+                    <div id="headerThree" className="card-header d-flex justify-content-between" data-toggle="collapse" href="#collapseThree">
+                      <button className="collapsed card-link text-dark">
+                        <i className="fa fa-user-o"></i>    My Friends
+                      </button>
+                      <KeyboardArrowDownIcon />
+                    </div>
+                    <div id="collapseThree" className={`collapse ${styles.accordionBoxBody}`} data-parent="#accordion">
+                      <div id="bodyThree" className={`card-body`}>
+                        <Friends />
+                      </div>
+                    </div>
                   </div>
-                </div>
-              </div>
 
-              {/* Make Friend Requests */}
-              <div className={`card my-3 ${styles.accordionBox}`}>
-                <div id="headerFive" className="card-header d-flex justify-content-between" data-toggle="collapse" href="#collapseFive">
-                  <button className="collapsed card-link text-dark">
-                    <i className="fa fa-user-o"></i>    Find Friends
-                  </button>
-                  <KeyboardArrowDownIcon/>
-                </div>
-                <div id="collapseFive" className={`collapse ${styles.accordionBoxBody}`} data-parent="#accordion">
-                  <div id="bodyFive" className={`card-body`}>
-                    <FindFriends />
+                  {/* Pending Friend Requests */}
+                  <div className={`card my-3 ${styles.accordionBox}`}>
+                    <div id="headerFour" className="card-header d-flex justify-content-between" data-toggle="collapse" href="#collapseFour">
+                      <button className="collapsed card-link text-dark">
+                        <i className="fa fa-user-o"></i>    My Pending Friend Requests
+                      </button>
+                      <KeyboardArrowDownIcon />
+                    </div>
+                    <div id="collapseFour" className={`collapse ${styles.accordionBoxBody}`} data-parent="#accordion">
+                      <div id="bodyFour" className={`card-body`}>
+                        <PendingFriends />
+                      </div>
+                    </div>
                   </div>
-                </div>
-              </div>
-              
+
+                </Box>
+              </Box>
             </Box>
           </Box>
-        </Box>
-      </Box>
 
       }
     </Fragment>
